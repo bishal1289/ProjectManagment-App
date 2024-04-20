a0c0efeb153286ed05b223ee4e54fe1cf51267fa
Bishal Mandal <106617899+bishal1289@users.noreply.github.com>
2024-04-20 18:55:08 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require('cors');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000","https://mern-pm-app.onrender.com"],
+}));
 
 const userRoutes = require("./routes/userRoute");
 const {connect} = require("./db/db");
